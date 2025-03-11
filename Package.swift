d28dfca57b6f63d4f4e2f41574a6297ca8fd2c83
Gleb-Kovalenko <80492878+Gleb-Kovalenko@users.noreply.github.com>
2025-03-11 16:32:18 +0300

    feat: add new version of source kitten adapter

--- Package.swift
+++ Package.swift
@@ -19,7 +19,7 @@ let package = Package(
     dependencies: [
         .package(
             url: "https://github.com/Incetro/source-kitten-adapter",
-            branch: "main"
+            from: "0.1.0"
         ),
         .package(
             url: "https://github.com/jpsim/SourceKitten",
